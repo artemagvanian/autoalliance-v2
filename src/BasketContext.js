39b06c8238b6b6c169e693e35bea2503341d545d
Artem Agvanian <artemagvanian@MacBook-Pro-Artem.local>
2020-05-10 17:21:27 +0300

    Change typo in basket context

--- src/BasketContext.js
+++ src/BasketContext.js
@@ -1,5 +1,5 @@
 import { createContext } from "react";
 
-const ThemeContext = createContext({ basket: [], handleBasketChange() {} });
+const BasketContext = createContext({ basket: [], handleBasketChange() {} });
 
-export default ThemeContext;
+export default BasketContext;
